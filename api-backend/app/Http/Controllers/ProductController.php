2dc9c3e9baa55ebc0e40ad0900f6026fda298cd9
gabriel <nenuco.1207@gmail.com>
2021-07-24 22:53:51 -0500

    Codificación del método index, el cual devuelve la lista de productos

--- api-backend/app/Http/Controllers/ProductController.php
+++ api-backend/app/Http/Controllers/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
 }
