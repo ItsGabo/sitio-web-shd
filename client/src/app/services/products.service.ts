7d886973be72c3e08147d94a83b3214d41ddad7a
Luis Angel <20183l301029@utcv.edu.mx>
2021-07-25 11:22:39 -0500

    Codificación del método que trae la lista de productos de la base de datos

--- client/src/app/services/products.service.ts
+++ client/src/app/services/products.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const apiUrl: string = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.executeQuery<Product>('/products');
+  }
+
+  private executeQuery<T>(query: string) {
+    
+    query = apiUrl + query;
+
+    return this.http.get<T>(query);
+  }
+}
+
+export interface Product {
+  id:          number;
+  name:        string;
+  description: string;
+  image:       string;
+  price:       string;
+  created_at:  Date;
+  updated_at:  Date;
 }
